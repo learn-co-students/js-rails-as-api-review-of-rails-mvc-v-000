741609d118f46b9e6d4d4b3ff860b3049e87c4f5
William Barela <william.barela.dev@gmail.com>
2020-02-06 10:51:09 -0600

    add modifications

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,1 +1,10 @@
 # Add code from Readme
+
+class BirdsController < ApplicationController
+  def index
+    @birds = Bird.all
+    # due to Rails convention over configuration, we don't need to type the following
+    # because there is a *folder* and a *file* corresponding to a *controller* and an *action* from that controller
+    # render 'birds/index.html.erb'
+  end
+end
