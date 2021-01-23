4e53f1742e3f46b32a35d30fc90e066d96f31dd3
yurigurlie207 <adrienne.yao@gmail.com>
2021-01-23 12:30:19 -0500

    Added json rendering to index method

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,9 @@
 # Add code from Readme
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
+    birds = Bird.all
+    #  render plain: "Hello #{@birds[3].name}"
+    # render json: @birds
+    render json: birds
   end
 end
